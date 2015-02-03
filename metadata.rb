ffc8d4108df130cd7aaa6730d7f9bf348b39169c
Peter Richardson <github@zoomy.net>
2015-02-03 15:53:55 -0500

    add stuff to chef it

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'buildingdb'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures buildingdb'
+long_description 'Installs/Configures buildingdb'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: buildingdb
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+file "/tmp/stuff" do
+  action :create
+end
