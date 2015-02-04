955e71f8d026474d62b7992f669b0102f3a540b4
Robin Hawkes <rob.hawkes@gmail.com>
2015-02-04 16:12:45 +0000

    Added new building index

--- mongo-setup.js
+++ mongo-setup.js
@@ -1,4 +1,5 @@
 // Create indexes
 db.users.ensureIndex( { email: 1 }, { unique: true } );
 db.users.ensureIndex( { username: 1 }, { unique: true } );
+db.buildings.ensureIndex( { slug: 1 }, { unique: true } );
 db.buildings.ensureIndex( { location: "2dsphere" } );
