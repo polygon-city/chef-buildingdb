f3d53659843ee32b7445a572f52a74665313843d
Robin Hawkes <rob.hawkes@gmail.com>
2015-02-05 12:51:20 +0000

    Updated indexes

--- mongo-setup.js
+++ mongo-setup.js
@@ -1,5 +1,5 @@
 // Create indexes
 db.users.ensureIndex( { email: 1 }, { unique: true } );
 db.users.ensureIndex( { username: 1 }, { unique: true } );
-db.buildings.ensureIndex( { slug: 1 }, { unique: true } );
+db.buildings.ensureIndex( { "slug.id": 1 }, { unique: true } );
 db.buildings.ensureIndex( { location: "2dsphere" } );
