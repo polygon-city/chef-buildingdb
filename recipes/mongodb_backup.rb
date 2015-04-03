3ce3c2beeebc3cbcae3e9b96fb163689bf719125
Baldur Gudbjornsson <baldur.gudbjornsson@gmail.com>
2015-04-03 18:34:26 -0400

    moving cron scrip to a file

--- recipes/mongodb_backup.rb
+++ recipes/mongodb_backup.rb
@@ -1,14 +1,20 @@
 package "python-pip"
 execute "pip install awscli"
 
+file "/usr/bin/mongodb_s3_backup" do
+  mode '0755'
+  content <<-EOM
+#! /bin/bash
+filename=$(date +%F-%s)
+backup_path=/data/db/backup/$filename
+/usr/bin/mongodump -o $backup_path
+/bin/tar -zcvf $filename.tar.gz $backup_path
+/usr/local/bin/aws s3 cp $filename.tar.gz s3://dev.polygon.city/backups/
+  EOM
+end
+
 cron "mongo_backup" do
   action :create
-  command %w{
-    filename=$(date +%F-%s)
-    backup_path=/data/db/backup/$filename
-    /usr/bin/mongodump -o $backup_path
-    /bin/tar -zcvf $filename.tar.gz $backup_path
-    /usr/local/bin/aws s3 cp $filename.tar.gz s3://dev.polygon.city/backups/
-  }.join(' ')
+  command "/usr/bin/mongodb_s3_backup"
 end
 
